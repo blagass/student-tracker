d47f9055e20c6f22b96e9ce9c8337c754aaed847
blagass <67036752+blagass@users.noreply.github.com>
2024-04-22 15:29:47 -0700

    First views and view switching set up

--- app/src/main/java/UI/MainActivity.java
+++ app/src/main/java/UI/MainActivity.java
@@ -1,6 +1,9 @@
 package UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button button=findViewById(R.id.startButton);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, TermsList.class);
+                intent.putExtra("test","This is a test.");
+                startActivity(intent);
+            }
         });
     }
 }
---
+++ app/src/main/java/UI/TermsList.java
@@ -0,0 +1,63 @@
+package UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tracker.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class TermsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_terms_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        FloatingActionButton fab=findViewById(R.id.addTermButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(TermsList.this,AddNewTerm.class);
+                startActivity(intent);
+            }
+        });
+
+        System.out.println(getIntent().getStringExtra("test"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_terms, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId()==R.id.courses){
+            Toast.makeText(this, "Course Selected?", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if(item.getItemId()==android.R.id.home){
+            this.finish();
+            return true;
+        }
+        return true;
+    }
+
+}
