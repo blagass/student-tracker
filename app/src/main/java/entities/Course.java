08080b43530e90b9f48b9246497f465de74e037d
blagass <67036752+blagass@users.noreply.github.com>
2024-04-22 21:58:34 -0700

    Two entities created, ready for database and dao

---
+++ app/src/main/java/entities/Course.java
@@ -0,0 +1,53 @@
+package entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "courses")
+public class Course {
+    @PrimaryKey(autoGenerate = true)
+    private int courseID;
+    private String courseName;
+    private String instructorName;
+    //Foreign key
+    private int termID;
+
+    public Course(int courseID, String courseName, String instructorName, int termID) {
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.instructorName = instructorName;
+        this.termID = termID;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public int getTermID() {
+        return termID;
+    }
+
+    public void setTermID(int termID) {
+        this.termID = termID;
+    }
+}
---
+++ app/src/main/java/entities/Term.java
@@ -0,0 +1,33 @@
+package entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+//New Terms entity table
+@Entity(tableName = "terms")
+public class Term {
+    @PrimaryKey(autoGenerate = true)
+    private int termID;
+    private String termName;
+
+    public Term(int termID, String termName) {
+        this.termID = termID;
+        this.termName = termName;
+    }
+
+    public int getTermID() {
+        return termID;
+    }
+
+    public void setTermID(int termID) {
+        this.termID = termID;
+    }
+
+    public String getTermName() {
+        return termName;
+    }
+
+    public void setTermName(String termName) {
+        this.termName = termName;
+    }
+}
