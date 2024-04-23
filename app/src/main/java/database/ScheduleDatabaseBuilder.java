9fef361ac55c57d6e8a8151e8d966b2e5963f4d9
blagass <67036752+blagass@users.noreply.github.com>
2024-04-22 22:29:45 -0700

    DB made

--- app/src/main/java/database/ScheduleDatabaseBuilder.java
+++ app/src/main/java/database/ScheduleDatabaseBuilder.java
@@ -1,4 +1,32 @@
 package database;
 
-public class ScheduleDatabaseBuilder {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import dao.CourseDAO;
+import dao.TermDAO;
+import entities.Course;
+import entities.Term;
+
+@Database(entities = {Term.class, Course.class}, version = 1, exportSchema = false) //Needs update everytime changes made to db
+public abstract class ScheduleDatabaseBuilder extends RoomDatabase {
+    public abstract TermDAO termDAO(); //Term dao
+    public abstract CourseDAO courseDAO(); //Course dao
+    private static volatile ScheduleDatabaseBuilder INSTANCE;
+
+    static ScheduleDatabaseBuilder getDatabase(final Context context){
+        if(INSTANCE==null){
+            synchronized (ScheduleDatabaseBuilder.class){
+                if(INSTANCE==null){
+                    INSTANCE= Room.databaseBuilder(context.getApplicationContext(),ScheduleDatabaseBuilder.class,"MyScheduleDatabase.db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
