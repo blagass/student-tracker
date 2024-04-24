ed7ba755efccb2acfa80a30284c9e39092e8a796
blagass <67036752+blagass@users.noreply.github.com>
2024-04-24 01:26:56 -0700

    Scrolling and terms nav done

--- app/src/main/java/UI/CourseDetails.java
+++ app/src/main/java/UI/CourseDetails.java
@@ -1,8 +1,6 @@
 package UI;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,24 +9,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.tracker.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class TermDetails extends AppCompatActivity {
+public class CourseDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_term_details);
+        setContentView(R.layout.activity_course_details);
 
-        FloatingActionButton fab = findViewById(R.id.addTermDetails);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(TermDetails.this, CourseDetails.class);
-                startActivity(intent);
-            }
-        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
