80af14a710832e552e653ca2467f5d56153daf92
blagass <67036752+blagass@users.noreply.github.com>
2024-05-06 11:21:30 -0700

    CourseDetails done

--- app/src/main/java/UI/CourseDetails.java
+++ app/src/main/java/UI/CourseDetails.java
@@ -1,6 +1,8 @@
 package UI;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.tracker.R;
 
+import database.Repository;
+import java.util.ArrayList;
+import java.util.List;
+
+import database.Repository;
+import entities.Course;
+import entities.Term;
+
 public class CourseDetails extends AppCompatActivity {
+    String name;
+    int courseID;
+    int termID;
+    EditText editName;
+    EditText editNote;
+    TextView editDate;
+    Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +35,16 @@ public class CourseDetails extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_course_details);
 
+        repository=new Repository(getApplication());
+
+        name= getIntent().getStringExtra("name");
+        //editName=findViewById(R.id.course);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
