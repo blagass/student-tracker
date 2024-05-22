99cf071f256445cf152799935871f1e61c509d0b
blagass <67036752+blagass@users.noreply.github.com>
2024-05-21 23:47:39 -0700

    aassessment type working!

--- app/src/main/java/UI/AssessmentDetails.java
+++ app/src/main/java/UI/AssessmentDetails.java
@@ -2,14 +2,24 @@ package UI;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tracker.R;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.List;
 
 import database.Repository;
 import entities.Assessment;
@@ -19,6 +29,10 @@ public class AssessmentDetails extends AppCompatActivity {
     String sAssessmentName;
     EditText etAssessmentName;
     Repository repository;
+    private String assessmentType;
+    private RadioGroup assessmentButtonGroup;
+    private RadioButton objectiveButton;
+    private RadioButton performanceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -38,7 +52,33 @@ public class AssessmentDetails extends AppCompatActivity {
         //settexts
         etAssessmentName.setText(sAssessmentName);
 
+        //TYPE
+        assessmentButtonGroup = findViewById(R.id.assessmentButtonGroup);
+        objectiveButton = findViewById(R.id.objectiveButton);
+        performanceButton = findViewById(R.id.performanceButton);
 
+        assessmentButtonGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.objectiveButton) {
+                assessmentType = "Objective";
+            } else if (checkedId == R.id.performanceButton) {
+                assessmentType = "Performance";
+            }
+        });
+        if (assessmentId != -1) {
+            Assessment assessment = repository.getmAllAssessments().stream()
+                    .filter(a -> a.getAssessmentId() == assessmentId)
+                    .findFirst()
+                    .orElse(null);
+
+            if (assessment != null) {
+                assessmentType = assessment.getAssessmentType(); // Get the assessment type
+                if (assessmentType.equals("Objective")) {
+                    objectiveButton.setChecked(true);
+                } else {
+                    performanceButton.setChecked(true);
+                }
+            }
+        }
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_assessmentdetails, menu);
@@ -47,7 +87,6 @@ public class AssessmentDetails extends AppCompatActivity {
 
     public boolean onOptionsItemSelected(MenuItem item){
         if(item.getItemId()==R.id.assessmentsave){
-
             Assessment assessment;
 
             if(assessmentId == -1){
@@ -56,12 +95,12 @@ public class AssessmentDetails extends AppCompatActivity {
                 else
                     assessmentId = repository.getmAllAssessments().get(repository.getmAllAssessments().size() -1).getAssessmentId() +1;
 
-                assessment = new Assessment(assessmentId, etAssessmentName.getText().toString());
+                assessment = new Assessment(assessmentId, etAssessmentName.getText().toString(),assessmentType);
                 repository.insert(assessment);
                 this.finish();;
             }
             else{
-                assessment = new Assessment(assessmentId, etAssessmentName.getText().toString());
+                assessment = new Assessment(assessmentId, etAssessmentName.getText().toString(),assessmentType);
                 repository.update(assessment);
                 this.finish();;
             }
--- app/src/main/java/entities/Assessment.java
+++ app/src/main/java/entities/Assessment.java
@@ -9,11 +9,17 @@ public class Assessment {
     @PrimaryKey(autoGenerate = true)
     private final int assessmentId;
     private final String assessmentName;
+    private final String assessmentType;
+
+    public String getAssessmentType() {
+        return assessmentType;
+    }
 
     //CONSTRUCTOR
-    public Assessment(int assessmentId, String assessmentName) {
+    public Assessment(int assessmentId, String assessmentName, String assessmentType) {
         this.assessmentId=assessmentId;
         this.assessmentName=assessmentName;
+        this.assessmentType=assessmentType;
     }
     //GETTERS
     public int getAssessmentId() {
